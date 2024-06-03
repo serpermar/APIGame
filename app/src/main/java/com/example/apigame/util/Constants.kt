6810ae529e14746bd41b23bd824d9aa2ae56aaba
sjo <sergi061202@gmail.com>
2024-06-03 19:26:27 +0200

    cambios de api

--- app/src/main/java/com/example/apigame/util/Constants.kt
+++ app/src/main/java/com/example/apigame/util/Constants.kt
@@ -6,7 +6,6 @@ class Constants {
         const val ENDPOINT = "games"
         const val API_KEY = "?key=ed3c80d674b54159a7b508736990c6dd"
 
-
         const val COLOR_1 = 0xFFEE9322.toInt() //orange
         const val COLOR_2 = 0xFFD83F31.toInt() //red
         const val COLOR_3 = 0xFF219C90.toInt() //blue/green
